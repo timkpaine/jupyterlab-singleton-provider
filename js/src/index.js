570a573484aae0c60ff51075b7166d651044b680
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2025-11-30 15:32:51 -0500

    Add extension

--- js/src/index.js
+++ js/src/index.js
@@ -1,17 +1,32 @@
+import {Token} from "@lumino/coreutils";
 import "../style/index.css";
 
-async function activate(app) {
-  // eslint-disable-next-line no-console
-  console.log("JupyterLab extension jupyterlab-singleton-provider is activated!");
+export const PLUGIN_ID = "jupyterlab-singleton-provider";
+export const ISingletonProvider = new Token(PLUGIN_ID);
+
+export class SingletonElement extends HTMLElement {
+  constructor() {
+    super();
+    this.innerHTML = `<div style="border: 2px solid blue; padding: 10px;">
+      <h3>Singleton Web Component</h3>
+      <p>This is a singleton web component provided by the jupyterlab-singleton-provider extension.</p>
+    </div>`;
+  }
 }
 
+// Define the custom element
+customElements.define("singleton-element", SingletonElement);
+
 const extension = {
-  activate,
+  activate: (app) => {
+    // eslint-disable-next-line no-console
+    console.log("JupyterLab extension jupyterlab-singleton-provider is activated!");
+  },
   autoStart: true,
-  id: "jupyterlab-singleton-provider",
+  id: PLUGIN_ID,
   optional: [],
   requires: [],
+  provides: [ISingletonProvider],
 };
 
 export default extension;
-export {activate as _activate};
--- js/tests/activate.test.js
+++ js/tests/activate.test.js
@@ -1,9 +1,10 @@
 import "isomorphic-fetch";
 
-import {_activate} from "../src/index";
+import {ISingletonProvider, PLUGIN_ID} from "../src/index";
 
 describe("Checks activate", () => {
   test("Check activate", () => {
-    expect(_activate);
+    expect(ISingletonProvider).toBeDefined();
+    expect(PLUGIN_ID).toBe("jupyterlab-singleton-provider");
   });
 });
